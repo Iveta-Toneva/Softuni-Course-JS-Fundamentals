bebf142546a25c958d07131cfe0c4beac515d81b
Iveta Toneva <iveta.v.toneva@gmail.com>
2024-02-22 15:59:01 +0000

    Basic Syntax, Conditional Statements and Loops

--- Basic Syntax, Conditional Statements and Loops/More Exercises/3-next-day.js
+++
@@ -1,9 +0,0 @@
-function nextDay(year, month, day) {
- let currentDate=new Date(year,month-1,day);
- let dayAfterOneDay=new Date(currentDate.setDate(currentDate.getDate()+1));
- let yearPrint=dayAfterOneDay.getFullYear();
- let monthPrint=dayAfterOneDay.getMonth()+1;
- let dayPrint=dayAfterOneDay.getDate();
- console.log(`${yearPrint}-${monthPrint}-${dayPrint}`); 
-}
-nextDay(2016, 9, 30);
