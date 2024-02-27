5c8b8c0260c12b25d6cdfed07e97efe079a1804c
Iveta Toneva <iveta.v.toneva@gmail.com>
2024-02-27 15:25:27 +0000

    Arrays

--- Arrays/Lab/8-condense-array-to-number.js
+++ Arrays/Lab/8-condense-array-to-number.js
@@ -1,6 +1,6 @@
 function condenseArray(array) {
   while (array.length !== 1) {
-    for (let index = 0; index < array.length; index++) {
+    for (let index = 0; index < array.length-1; index++) {
       array[index] = array[index] + array[index + 1];
     }
     array = array.slice(0, -1);
