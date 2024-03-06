ccf52f4ad05ee9377ce746497cedb61b0c57a838
Iveta-Toneva <160541425+Iveta-Toneva@users.noreply.github.com>
2024-03-06 17:51:48 +0000

    Update 6-sign-check-no-calculations.js

--- Functions/Lab/6-sign-check-no-calculations.js
+++ Functions/Lab/6-sign-check-no-calculations.js
@@ -1,16 +1,4 @@
-// function signCheck(number1, number2, number3) {
-//   let result = number1 * number2 * number3;
-//   print(result);
 
-//   function print(result) {
-//     if (result > 0) {
-//       console.log("Positive");
-//     } else {
-//       console.log("Negative");
-//     }
-//   }
-// }
-// signCheck(-1, -2, -3);
 function signCheck(number1, number2, number3) {
   let result = "";
   if (number1 > 0 && number2 > 0 && number3 > 0) {
